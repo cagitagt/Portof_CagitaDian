a99d46e377dd3b5a4041f4a78b583213f754de1a
cagita dian <cagitadianayunin@gmail.com>
2025-07-11 20:57:33 +0700

    fixed first

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -5,38 +5,38 @@ const works = [
   {
     imgSrc: '/images/batikindo.png',
     title: 'E-Commerce Website',
-    tags: ['Vue.js'],
-    projectLink: 'https://github.com/cagitagt/fe_rev_batik'
+    tags: ['Frontend', 'Nuxt.js', 'TailwindCSS'],
+    projectLink: 'https://batikindonesiaweb.vercel.app/'
   },
   {
     imgSrc: '/images/tictactoe.png',
     title: 'TicTacToe Game',
-    tags: ['JS'],
+    tags: ['Frontend', 'JS', 'Native'],
     projectLink: 'https://gmticctactoe.netlify.app/'
   },
   {
     imgSrc: '/images/flixera.png',
     title: 'Streaming Service',
-    tags: ['Development', 'API'],
-    projectLink: ''
+    tags: ['Frontend', 'Vue.js', 'TailwindCSS'],
+    projectLink: 'https://flixeraweb.vercel.app/'
   },
   {
-    imgSrc: '/images/project-4.jpg',
-    title: 'Real state website',
-    tags: ['Web-design', 'Development'],
-    projectLink: 'https://github.com/codewithsadee-org/wealthome'
+    imgSrc: '/images/safaroutes.png',
+    title: 'Travel App',
+    tags: ['Mobile-design', 'UI/UX', 'Figma'],
+    projectLink: 'https://www.figma.com/proto/Yjm4QSDIUhT3j3ZK4f1fs6/Safaroutes?page-id=0%3A1&node-id=4-2603&p=f&viewport=87%2C-333%2C0.28&t=UgrdTSx8rQzRBjUM-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=4%3A2603&show-proto-sidebar=1'
   },
   {
-    imgSrc: '/images/project-5.jpg',
-    title: 'eCommerce website',
-    tags: ['eCommerce', 'Development'],
-    projectLink: 'https://github.com/codewithsadee/anon-ecommerce-website'
+    imgSrc: '/images/feelty.png',
+    title: 'Sports App',
+    tags: ['Mobile-design', 'UI/UX', 'Figma'],
+    projectLink: 'https://www.figma.com/proto/yM7cvJGTNHP8C8ttVUoDfv/Technotion?page-id=20%3A2&node-id=307-1587&viewport=407%2C812%2C0.17&t=TmEMl2AvmomvRiBw-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=335%3A1907&show-proto-sidebar=1'
   },
   {
-    imgSrc: '/images/project-6.jpg',
-    title: 'vCard Personal portfolio',
-    tags: ['Web-design', 'Development'],
-    projectLink: 'https://github.com/codewithsadee/vcard-personal-portfolio'
+    imgSrc: '/images/creanomic.png',
+    title: 'Promotion Website',
+    tags: ['Web-design', 'UI/UX', 'Figma'],
+    projectLink: 'https://www.creanomicub.com/'
   },
 ];
 
